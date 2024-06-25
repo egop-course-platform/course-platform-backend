339b4a41940f15245e495d8cd1e48c97821541de
bird_egop <sampletext32@bk.ru>
2024-06-25 19:42:08 +0300

    add mirrorsharp

--- Cp.Presentation/Program.cs
+++ Cp.Presentation/Program.cs
@@ -1,6 +1,18 @@
+using MirrorSharp;
+using MirrorSharp.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapMirrorSharp(
+    "/mirrorsharp",
+    new MirrorSharpOptions()
+    {
+        IncludeExceptionDetails = true,
+        SelfDebugEnabled = true
+    }
+);
 
 app.Run();
